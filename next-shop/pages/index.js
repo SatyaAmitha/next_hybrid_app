ba5b6b36e660a58c324b933174f412d306f5918b
AMITHA\Dell <satyaamithachavakula@gmail.com>
2024-11-16 15:29:52 +0530

    Part 30: Building a Hybrid Shop Website with Next.js

---
+++ next-shop/pages/index.js
@@ -0,0 +1,17 @@
+// next-shop/pages/index.js
+import Head from 'next/head';
+
+function HomePage() {
+  return (
+    <>
+      <Head>
+        <title>Next Shop</title>
+      </Head>
+      <main>
+        <h1>Next Shop</h1>
+      </main>
+    </>
+  );
+}
+
+export default HomePage;
