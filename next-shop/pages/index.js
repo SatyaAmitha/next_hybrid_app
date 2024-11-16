e2fa2fc2a0cc641aab3ed1561f9edecd387e7c5f
AMITHA\Dell <satyaamithachavakula@gmail.com>
2024-11-16 15:44:54 +0530

    Part 32: Styling with Tailwind CSS in Your Next.js Project

--- next-shop/pages/index.js
+++ next-shop/pages/index.js
@@ -1,14 +1,19 @@
-// next-shop/pages/index.js
+// pages/index.js
 import Head from 'next/head';
+import Title from '../components/Title';
 
 function HomePage() {
   return (
     <>
       <Head>
-        <title>Next Shop</title>
+        <title>Next</title>
       </Head>
-      <main>
-        <h1>Next Shop</h1>
+      <main className="px-6 py-4">
+        <Title>Next Shop</Title>
+        <h4 className="text-black text-xl font-medium">Welcome</h4>
+        <p>
+          [TODO: display products]
+        </p>
       </main>
     </>
   );
