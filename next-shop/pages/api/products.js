89ad855289fc596685d05d2609b4ae5b5518e002
AMITHA\Dell <satyaamithachavakula@gmail.com>
2024-11-18 12:37:13 +0530

    Part 41: Enhancing Client-Side Data Fetching with Next.js API Routes

---
+++ next-shop/pages/api/products.js
@@ -0,0 +1,11 @@
+// pages/api/products.js
+
+import { getProducts } from '../../lib/products';
+
+async function handler(req, res) {
+  console.log('[/api/products] handler');
+  const products = await getProducts();
+  res.status(200).json(products);
+}
+
+export default handler;
