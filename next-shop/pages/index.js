e33df3fef8b1394b104741dae39fda0442168fa0
AMITHA\Dell <satyaamithachavakula@gmail.com>
2024-11-18 11:45:00 +0530

    Part 36: Fetching Product Data in Next.js: A Tale of Two Approaches

--- next-shop/pages/index.js
+++ next-shop/pages/index.js
@@ -1,19 +1,29 @@
 // pages/index.js
+
 import Head from 'next/head';
 import Title from '../components/Title';
 
+const products = [
+  { id: 1, title: 'First Product' },
+  { id: 2, title: 'Second Product' },
+];
+
 function HomePage() {
+  console.log('[HomePage] render:', products);
   return (
     <>
       <Head>
-        <title>Next</title>
+        <title>Next Shop</title>
       </Head>
       <main className="px-6 py-4">
         <Title>Next Shop</Title>
-        <h4 className="text-black text-xl font-medium">Welcome</h4>
-        <p>
-          [TODO: display products]
-        </p>
+        <ul>
+          {products.map((product) => (
+            <li key={product.id}>
+              {product.title}
+            </li>
+          ))}
+        </ul>
       </main>
     </>
   );
