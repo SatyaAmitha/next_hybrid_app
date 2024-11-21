b85fddad133e4d2b52ac01dcdfff17bbb1b4736d
AMITHA\Dell <satyaamithachavakula@gmail.com>
2024-11-21 14:23:06 +0530

    Part 56: Optimizing Product Images with Next.js: A Guide

--- next-shop/components/ProductCard.js
+++ next-shop/components/ProductCard.js
@@ -1,12 +1,13 @@
 // components/ProductCard.js
 
+import Image from 'next/image';
 import Link from 'next/link';
 
 function ProductCard({ product }) {
   return (
     <div className="border w-80 shadow hover:shadow-xl">
       <Link href={`/products/${product.id}`}>
-        <img src={product.pictureUrl} alt={product.title} />
+        <Image src={product.pictureUrl} alt={product.title} width={320} height={240} />
         <div className="p-2 flex justify-between items-baseline">
           <h2 className="text-lg font-bold">
             {product.title}
--- next-shop/next.config.mjs
+++ next-shop/next.config.mjs
@@ -1,6 +1,11 @@
+// next.config.js
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['localhost'], // Add your image source domain(s) here
+  },
 };
 
 export default nextConfig;
