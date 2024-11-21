d214692d2a70c25ceee5c46a09ee839a2818b084
AMITHA\Dell <satyaamithachavakula@gmail.com>
2024-11-21 12:50:14 +0530

    Part 48: Harnessing Incremental Static Regeneration and Fallback Options in Next.js

--- next-shop/pages/products/[id].js
+++ next-shop/pages/products/[id].js
@@ -9,7 +9,7 @@ export async function getStaticPaths() {
     paths: products.map((product) => ({
       params: { id: product.id.toString() },
     })),
-    fallback: false,
+    fallback: 'blocking',
   };
 }
 
