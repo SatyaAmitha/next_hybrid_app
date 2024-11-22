6f540d791d105ed211e7f05211a7d8535a574ecf
AMITHA\Dell <satyaamithachavakula@gmail.com>
2024-11-22 11:22:10 +0530

    Part 66: Enhancing Your API with Secure Cookie Management and Environment Variables

--- next-shop/pages/api/login.js
+++ next-shop/pages/api/login.js
@@ -1,6 +1,9 @@
 // pages/api/login.js
 
+import cookie from 'cookie';
 import { fetchJson } from '../../lib/api';
+
+const { CMS_URL } = process.env;
 
 async function handleLogin(req, res) {
   if (req.method !== 'POST') {
@@ -11,18 +14,23 @@ async function handleLogin(req, res) {
   const { email, password } = req.body;
 
   try {
-    const { jwt, user } = await fetchJson('http://localhost:1337/auth/local', {
+    const { jwt, user } = await fetchJson(`${CMS_URL}/auth/local`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ identifier: email, password }),
     });
 
-    // TODO: Set jwt cookie
+    // TODO: Set JWT cookie
 
-    res.status(200).json({
-      id: user.id,
-      name: user.username,
-    });
+    res.status(200)
+      .setHeader('Set-Cookie', cookie.serialize('jwt', jwt, {
+        path: '/api',
+        httpOnly: true,
+      }))
+      .json({
+        id: user.id,
+        name: user.username,
+      });
   } catch (err) {
     res.status(401).end(); // Unauthorized
   }
